a95c6d44681816c74036b5fc59788e0fb09894fa
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-06-28 15:39:56 +0200

    Condition on price added

--- js/script.js
+++ js/script.js
@@ -4,3 +4,8 @@ chilometri = Number(chilometri);
 eta = Number(eta);
 let prezzoPerKm = 0.21;
 let prezzoTotale = chilometri * prezzoPerKm;
+if (eta < 18) {
+    prezzoTotale *= 0.8;
+} else if (eta > 65) {
+    prezzoTotale *= 0.6;
+}
