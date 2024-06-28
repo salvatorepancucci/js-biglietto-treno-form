5701c88da1db93f2ff16a1b5deab7affdfae4764
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-06-28 15:45:48 +0200

    Esercizio completato

--- js/script.js
+++ js/script.js
@@ -4,13 +4,20 @@ chilometri = Number(chilometri);
 eta = Number(eta);
 let prezzoPerKm = 0.21;
 let prezzoTotale = chilometri * prezzoPerKm;
-if (eta < 18) {
-    prezzoTotale *= 0.8;
-} else if (eta > 65) {
-    prezzoTotale *= 0.6;
+function calcolaPrezzo() {
+    chilometri = Number(document.getElementById("chilometri").value);
+    eta = Number(document.getElementById("eta").value);
+    prezzoTotale = chilometri * prezzoPerKm;
+
+    if (eta < 18) {
+        prezzoTotale *= 0.8;
+    } else if (eta > 65) {
+        prezzoTotale *= 0.6;
+    }
+
+    prezzoTotale = prezzoTotale.toFixed(2);
+    document.getElementById("risultato").innerText = `Il prezzo finale del biglietto è €${prezzoTotale}`;
 }
-prezzoTotale = prezzoTotale.toFixed(2);
-document.getElementById("risultato").innerText = `Il prezzo finale del biglietto è €${prezzoTotale}`;
 function resetForm() {
     document.getElementById("risultato").innerText = "";
 }
