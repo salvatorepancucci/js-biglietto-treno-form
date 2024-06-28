0c15f9dbee7e080078a495308928134e8b9133b3
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-06-28 15:39:37 +0200

    Defined price variables

--- js/script.js
+++ js/script.js
@@ -2,3 +2,5 @@ let chilometri = document.getElementById("chilometri").value;
 let eta = document.getElementById("eta").value;
 chilometri = Number(chilometri);
 eta = Number(eta);
+let prezzoPerKm = 0.21;
+let prezzoTotale = chilometri * prezzoPerKm;
